20ecf516fc5b63583fe899009b3e7534305d9b9d
kelsey langlois and LDMacKrell <ldmackrell@>
2017-09-05 14:10:19 -0700

    Update queen_attack to return true when positions are in line horizontally.

--- lib/queen_attack.rb
+++ lib/queen_attack.rb
@@ -1,5 +1,9 @@
 class Array
   def queen_attack?(target)
-    false
+    if self[1] == target[1]
+      return true
+    else
+      return false
+    end
   end
 end
--- spec/queen_attack_spec.rb
+++ spec/queen_attack_spec.rb
@@ -5,4 +5,8 @@ describe('#queen_attack?') do
   it("returns false if the coordinates are not horizontally, vertically, or diagonally in line with each other") do
     expect([1,1].queen_attack?([2,3])).to(eq(false))
   end
-end 
+
+  it("returns true if the coordinates are horizontally in line with each other") do
+    expect([4,1].queen_attack?([6,1])).to(eq(true))
+  end
+end
